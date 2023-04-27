841d647542f2500e0045760a13427c7f9f093f4b
Yesika Rodearni <68741697+yesikarodearni@users.noreply.github.com>
2023-04-27 23:59:46 +0700

    Update Kel3Tgs3.java

--- TugasKelompok1/src/Kel3Tgs3.java
+++ TugasKelompok1/src/Kel3Tgs3.java
@@ -12,7 +12,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-public class Kel3Tgs3
-{
-    
+
+public class Kel3Tgs3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pilihan;
+
+        do {
+            System.out.println("Coepoe Word Puzzle");
+            System.out.println("==================");
+            System.out.println("");
+            System.out.println("Rules :");
+            System.out.println("1. Create a word using given characters, min 3 characters & max 6 characters.");
+            System.out.println("2. Each level, You have 10 chances to answer correctly.");
+            System.out.println("3. To get through to next level, you have to score 70 points each level.");
+            System.out.println("");
+            System.out.println("Level 1");
+            System.out.println("=======");
+
+            // Pilihan Ulang Program
+            System.out.print("Anda ingin mengulang (Y/T) : ");
+            input.nextLine();
+            char ulang = input.nextLine().charAt(0);
+            pilihan = Character.toUpperCase(ulang);
+        } while (pilihan == 'Y');
+
+    }
 }
