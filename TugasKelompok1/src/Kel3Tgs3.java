a9a8d6794c1bf0f8b51ad9173d6a439fe72e4358
FachryAzri <46189875+FachryAzriA@users.noreply.github.com>
2023-04-27 21:47:10 +0700

    add file baru

---
+++ TugasKelompok1/src/Kel3Tgs3.java
@@ -0,0 +1,18 @@
+/*
+    Kelompok 3 Tugas 2
+    - Fachry Azri Arrasyid (2602265211)
+    - Nesta Waldemar Binardo Tambunan (2602265136)
+    - Renanda Diva Acereistanti (2602269986)
+    - Fahrudin Aris Wicaksono (2602269260)
+    - Yesika Rodearni Simarmata (2602270073)
+*/
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Kel3Tgs3
+{
+    
+}
