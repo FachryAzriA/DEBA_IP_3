1dc0392e88c5caac3304976652f16cafef5d776a
Yesika Rodearni <68741697+yesikarodearni@users.noreply.github.com>
2023-05-03 23:02:08 +0700

    Create Kel3Tgs4.java

---
+++ TugasKelompok1/src/Kel3Tgs4.java
@@ -0,0 +1,25 @@
+/*
+    Kelompok 3 Tugas 4
+    - Fachry Azri Arrasyid (2602265211)
+    - Nesta Waldemar Binardo Tambunan (2602265136)
+    - Renanda Diva Acereistanti (2602269986)
+    - Fahrudin Aris Wicaksono (2602269260)
+    - Yesika Rodearni Simarmata (2602270073)
+*/
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Kel3Tgs4 {
+    public static void main(String[] args) {
+
+        // inisialisasi penggunaan buffer reader untuk string
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // inisialisasi penggunaan scanner untuk integer
+        Scanner scanner = new Scanner(System.in);
+
+        }
+}
